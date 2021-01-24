3415e92f1057b394fab8f27f204a2fd5dbc80f44
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-01-24 23:18:27 +0300

    Updated project. Added ArrayIterator.java. Added MyArrayList.java. Added MyArrayListImpl.java. Added ArrayIterator.class. Added MyArrayList.class. Added MyArrayListImpl.class. Added uiDesigner.xml. Updated pom.xml.

---
+++ src/main/java/by/maksim/arraylist/MyArrayList.java
@@ -0,0 +1,9 @@
+package by.maksim.arraylist;
+
+public interface MyArrayList<E> extends Iterable<E> {
+    boolean add(E e);
+    void delete(int index);
+    E get(int index);
+    int size();
+    void update(int index, E e);
+}
---
+++ src/main/java/by/maksim/arraylist/MyArrayListImpl.java
@@ -0,0 +1,70 @@
+package by.maksim.arraylist;
+
+import java.util.Iterator;
+
+public class MyArrayListImpl<E> implements MyArrayList<E> {
+
+    public static void main(String[] args) {
+        MyArrayListImpl<String> strings = new MyArrayListImpl<>();
+        strings.add("one");
+        strings.add("two");
+        strings.add("three");
+        for (String s :
+                strings) {
+            System.out.println(s);
+        }
+    }
+
+    private E[] values;
+
+    MyArrayListImpl() {
+        values = (E[]) new Object[0];
+    }
+
+    @Override
+    public boolean add(E e) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            values[values.length - 1] = e;
+            return true;
+        } catch (ClassCastException exception) {
+            exception.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void delete(int index) {
+        try {
+            E[] temp = values;
+            values = (E[]) new Object[temp.length - 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            int amountElementAfterIndex = temp.length - index - 1;
+            System.arraycopy(temp, index + 1, values, index, amountElementAfterIndex);
+        } catch (ClassCastException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public E get(int index) {
+        return values[index];
+    }
+
+    @Override
+    public int size() {
+        return values.length;
+    }
+
+    @Override
+    public void update(int index, E e) {
+        values[index] = e;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new ArrayIterator<>(values);
+    }
+}
